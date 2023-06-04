cbdba7834494fc167b5b2956538b3e29f8e3fc2a
Admin <Badr.Ouaddah.07@gmail.com>
2023-06-04 20:25:30 +0100

    Create multiple until tests

--- src/test/java/org/example/SimpleCalculatorTest.java
+++ src/test/java/org/example/SimpleCalculatorTest.java
@@ -10,6 +10,15 @@ class SimpleCalculatorTest {
     void twoPlusTwo() {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         assertEquals(4,simpleCalculator.add(2,2));
-
+    }
+    @Test
+    void threePlusSeven() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        assertEquals(10,simpleCalculator.add(3,7));
+    }
+    @Test
+    void fourPlusFour() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        assertEquals(8,simpleCalculator.add(4,4));
     }
 }
